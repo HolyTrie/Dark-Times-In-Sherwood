50c8798f28a53a2342b48359d7afd4c7ff1a1c9f
20shaked20 <shakedlevi32@gmail.com>
2024-01-28 12:09:21 +0200

    player can jump on enemy + shoot him in the head and he will die according to the force applied by impulse

---
+++ Assets/Scripts/Entities/Enemies/EntityBoundedMovement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+/**
+ *  This component moves its object in a fixed speed back and forth between two points in space.
+ TODO -> seperate into two - moving enemy + moving platform.
+ */
+public class EntityBoundedMovement : MonoBehaviour
+{
+    [Tooltip("The points between which the platform moves")]
+    [SerializeField] Transform startPoint = null, endPoint = null;
+    [SerializeField] float speed = 1f;
+    bool moveFromStartToEnd = true;
+
+    private void Start()
+    {
+        transform.position = startPoint.position;
+    }
+
+    void Update()
+    {
+        Flip();
+    }
+
+    void FixedUpdate()
+    {
+        // If Update is used, the player does not move with the platform.
+        float deltaX = speed * Time.fixedDeltaTime;
+        if (moveFromStartToEnd)
+        {
+            Debug.Log("Moves To End");
+            transform.position = Vector3.MoveTowards(transform.position, endPoint.position, deltaX);
+        }
+        else
+        {  // move from end to start
+            Debug.Log("Moves To Start");
+            transform.position = Vector3.MoveTowards(transform.position, startPoint.position, deltaX);
+        }
+    }
+
+    public void setDirection(bool Direction)
+    {
+        this.moveFromStartToEnd = Direction;
+    }
+
+    private void Flip()
+    {
+        if (moveFromStartToEnd)
+            transform.GetComponent<SpriteRenderer>().flipX = false;
+        else
+            transform.GetComponent<SpriteRenderer>().flipX = true;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // if (other.gameObject.GetComponent<KeyboardMover>()) {
+        //     other.transform.parent = this.transform;
+        // }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        // if (other.gameObject.GetComponent<KeyboardMover>()) {
+        //     other.transform.parent = null;
+        // }
+    }
+}
