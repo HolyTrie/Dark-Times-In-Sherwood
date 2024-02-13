29c4c06a88c86697077f3ff9c27a2a5a4387f180
Yonatan Ratner <yonaratner@gmail.com>
2024-02-13 15:40:07 +0200

    gate puzzle almost done

--- Assets/GateController.cs
+++ Assets/GateController.cs
@@ -1,19 +1,22 @@
 using System;
 using System.Collections;
 using DTIS;
+using Unity.VisualScripting;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 public class GateController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float _GateSegmentOpenDelaySeconds = 0.25f;
     private SpriteRenderer[] _SpriteChilds; 
+    private Collider2D _collider;
 
     void Awake()
     {
-        int i = 0;
+        _collider = GetComponent<Collider2D>();
         _SpriteChilds = new SpriteRenderer[transform.childCount];
 
-        for(i=0;i<_SpriteChilds.Length;++i)
+        for(int i=0;i<_SpriteChilds.Length;++i)
 		{
 			_SpriteChilds[i] = transform.GetChild(i).GetComponent<SpriteRenderer>(); 
         }
@@ -22,16 +25,18 @@ public class GateController : MonoBehaviour
 
     public void OpenGate()
     {
-        for(int i = _SpriteChilds.Length - 1 ; i >= 0 ; --i)
-        {
-            StartCoroutine(WaitForSprite(i));
-            
-        }
+        StartCoroutine(OpenOneByOne(_GateSegmentOpenDelaySeconds));
     }
 
-    private IEnumerator WaitForSprite(int idx)
+    private IEnumerator OpenOneByOne(float delay)
     {
-        yield return new WaitForSeconds(0.5f);
-        _SpriteChilds[idx].enabled = false;
+        for(int i = _SpriteChilds.Length - 1 ; i >= 0 ; --i)
+        {
+            yield return new WaitForSeconds(delay);
+            if(i!=0)
+                _SpriteChilds[i].transform.position = Vector3.Lerp(_SpriteChilds[i].transform.position,_SpriteChilds[i-1].transform.position, 0.5f);
+            _SpriteChilds[i].enabled = false;       
+        }
+        _collider.enabled = false; //finally turn off the collision.
     }
 }
