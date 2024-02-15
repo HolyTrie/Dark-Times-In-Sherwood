a25e2d47fc3bfebbac6ea70b9f71d243e01ffc0e
20shaked20 <shakedlevi32@gmail.com>
2024-02-16 01:20:14 +0200

    Fixed arrow issues - now the player cant shoot unless animation is done

--- Assets/Scripts/Entities/Player/PlayerController.cs
+++ Assets/Scripts/Entities/Player/PlayerController.cs
@@ -170,6 +170,16 @@ namespace DTIS
                 }
             }
         }
+
+        //this method should check if a certain animation is still playing (like shooting, if so DO NOT SHOOT)
+        public bool isPlaying(string stateName)
+        {
+            if (_animator.GetCurrentAnimatorStateInfo(0).IsName(stateName) &&
+                    _animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+                return true;
+            else
+                return false;
+        }
     }
 }
 
--- Assets/Scripts/Entities/Player/PlayerState/States/GroundedState.cs
+++ Assets/Scripts/Entities/Player/PlayerState/States/GroundedState.cs
@@ -18,16 +18,22 @@ namespace DTIS
             {
                 SetStates(ESP.States.Airborne, ESP.States.Jump);
             }
-            if (ActionMap.Shoot.WasPressedThisFrame() && !isShooting)
+            if (ActionMap.Shoot.IsPressed() && !isShooting)
             {
                 isShooting = true;
                 float offset = 3f;
                 Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - FSM.Controls.transform.localPosition;
                 //Debug.Log("Mouse Position: "+ dir + "PlayerPosition: "+ FSM.Controls.transform.localPosition );
                 if (dir.y - offset > FSM.Controls.transform.localPosition.y) // aiming above head
-                    SetStates(ESP.States.Grounded, ESP.States.HighAttackState);
-                else
-                    SetStates(ESP.States.Grounded, ESP.States.RangedAttack);
+                {
+                    if (!Controller.isPlaying("HighAttack"))
+                        SetStates(ESP.States.Grounded, ESP.States.HighAttackState);
+                }
+                else if (dir.y - offset <= FSM.Controls.transform.localPosition.y)
+                {
+                    if (!Controller.isPlaying("RangedAttack"))
+                        SetStates(ESP.States.Grounded, ESP.States.RangedAttack);
+                }
             }
             else
             {
