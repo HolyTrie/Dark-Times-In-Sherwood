4fd9d66d5fb92ba0c170029bef939007ec6bcd6e
Yonatan Ratner <yonaratner@gmail.com>
2024-02-19 18:34:21 +0200

    perfect spritesheet + gravity and collision working

--- Assets/PhysicsObject.cs
+++ Assets/PhysicsObject.cs
@@ -8,23 +8,27 @@ public class PhysicsObject : MonoBehaviour
     // source - https://www.youtube.com/watch?v=wGI2e3Dzk_w&list=PLX2vGYjWbI0SUWwVPCERK88Qw8hpjEGd8&index=1&ab_channel=Unity
     protected const float _minMoveDistance = 0.001f;
     protected const float _shellRadius = 0.01f;
+
     [SerializeField] private float _gravityModifier = 1f;
+    [SerializeField] private float _minGroundNormalY = 0.65f;
+
+    protected bool grounded;
     protected Rigidbody2D _rb2d;
     protected Vector2 _velocity;
+    protected Vector2 _groundNormal;
     protected ContactFilter2D _contactFilter2d;
     protected RaycastHit2D[] _hitBuffer = new RaycastHit2D[16]; // todo - variable length?
     protected List<RaycastHit2D> _hitBufferList = new(16);
+    
     protected void OnEnable() {
         _rb2d = GetComponent<Rigidbody2D>();
     }
     protected void Start()
     {
         _contactFilter2d.useTriggers = false;
-        _contactFilter2d.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer)); // get the attached game objects collision mask to filter by.
+        _contactFilter2d.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer)); // get the Physics@d collision mask for this layer to filter by.
         _contactFilter2d.useLayerMask = true;
     }
-
-    // Update is called once per frame
     protected void Update()
     {
         
@@ -32,22 +36,50 @@ public class PhysicsObject : MonoBehaviour
 
     protected void FixedUpdate()
     {
-        //TODO: compare with Time.fixedDetlaTime !
-        _velocity += _gravityModifier * Physics2D.gravity * Time.deltaTime;
+        _velocity += Time.deltaTime * _gravityModifier * Physics2D.gravity;
+        grounded = false;
         Vector2 deltaPostion = _velocity * Time.deltaTime;
         Vector2 move = Vector2.up * deltaPostion.y;
-        Movement(move);
+        Movement(move, true); // yMovement = true
     }
 
-    void Movement(Vector2 move)
+    void Movement(Vector2 move, bool yMovement)
     {
         float distance = move.magnitude;
+
         if (distance > _minMoveDistance)
         {
-            int rayCount = _rb2d.Cast(move, _contactFilter2d, _hitBuffer, distance + _shellRadius);
+            int count = _rb2d.Cast(move, new ContactFilter2D(), _hitBuffer, distance + _shellRadius); // stores results into _hitBuffer and returns its length (can be discarded).
+            _hitBufferList.Clear();
+            for(int i = 0; i < count; ++i) // DO NOT Refactor this with foreach! it will iterate over empty spaces.
+            {
+                _hitBufferList.Add(_hitBuffer[i]);
+            }
+            foreach(var hit in _hitBufferList)
+            {
+                Vector2 currentNormal = hit.normal;
+                if(currentNormal.y > _minGroundNormalY) // if the normal vectors angle is greater then the set value.
+                {
+                    grounded = true;
+                    if(yMovement)
+                    {
+                        _groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+
+                float projection = Vector2.Dot(_velocity, currentNormal); // differnece between velocity and currentNormal to know how much to subtract if the player collides with a wall/ceiling
+                if(projection < 0)
+                {
+                    _velocity -= projection * currentNormal;
+                }
+
+                float modifiedDistance = hit.distance - _shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
 
 
         }
-        _rb2d.position = _rb2d.position + move;
+        _rb2d.position += move.normalized * distance;
     }
 }
