ec3a12dd949e6531f07edd02e697f8fe67c4201a
20shaked20 <shakedlevi32@gmail.com>
2024-03-05 11:00:23 +0200

    Cursor fixes

--- Assets/Scripts/CrosshairCursor.cs
+++ Assets/Scripts/CrosshairCursor.cs
@@ -2,29 +2,33 @@ using UnityEngine;
 
 public class CrosshairCursor : MonoBehaviour
 {
-    [SerializeField] private Sprite BowSprite;
-    [SerializeField] private Sprite SwordSprite;
+    [SerializeField] private Texture2D BowSprite;
+    [SerializeField] private Texture2D SwordSprite;
+
     private SpriteRenderer _cursorSprite;
+    
     void Awake()
     {
-        _cursorSprite = GetComponent<SpriteRenderer>();
-        Cursor.visible = false;
+        // _cursorSprite = GetComponent<SpriteRenderer>();
+        // Cursor.visible = false;
     }
 
     void Update()
     {
-        Vector2 mouseCursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = mouseCursorPos;
+        // Vector2 mouseCursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // transform.position = mouseCursorPos;
     }
 
     public void BowCrosshair()
     {
-        _cursorSprite.sprite = BowSprite;
+        // _cursorSprite.sprite = BowSprite;
+        Cursor.SetCursor(BowSprite,Vector2.zero, CursorMode.ForceSoftware);
     }
 
     public void SwordCrosshair()
     {
-        _cursorSprite.sprite = SwordSprite;
+        // _cursorSprite.sprite = SwordSprite;
+        Cursor.SetCursor(SwordSprite,Vector2.zero, CursorMode.ForceSoftware);
     }
 
     
