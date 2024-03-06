b9a0c2fc17621e40826d6b891fa2cc121b19e508
20shaked20 <shakedlevi32@gmail.com>
2024-03-06 19:52:22 +0200

    Fixes - UI, Enemy Attacks perfected for raycasting, some bugs

--- Assets/Scripts/Entities/BehaviorTree/AINodeTasks/TaskAttackRaycast.cs
+++ Assets/Scripts/Entities/BehaviorTree/AINodeTasks/TaskAttackRaycast.cs
@@ -1,4 +1,5 @@
 using DTIS;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TaskAttackRaycast : MonoBehaviour
@@ -11,12 +12,14 @@ public class TaskAttackRaycast : MonoBehaviour
 
     [SerializeField] private float AttackRadius = 0.5f;
 
+    private RaycastHit2D hit;
     public void DamagePlayer()
     {
-        var hit = Physics2D.CircleCast(HitTransform.position, AttackRadius, Vector3.forward, 1, 1 << 3); // player layer.
+        hit = Physics2D.CircleCast(HitTransform.position, AttackRadius, Vector3.forward, 1, 1 << 3); // player layer.
         if (hit)
         {
-            hit.transform.gameObject.TryGetComponent(out PlayerController playerC);
+            // hit.transform.gameObject.TryGetComponent(out PlayerController playerC);
+            PlayerController playerC = Util.GetPlayerController();
             if (playerC != null)
             {
                 playerC.HpBar.depleteHp(DMG);
@@ -26,7 +29,7 @@ public class TaskAttackRaycast : MonoBehaviour
 
     public void DamageEnemy()
     {
-        var hit = Physics2D.CircleCast(HitTransform.position, AttackRadius, Vector3.forward, 1, 1 << 10); // Enemy layer.
+        hit = Physics2D.CircleCast(HitTransform.position, AttackRadius, Vector3.forward, 1, 1 << 10); // Enemy layer.
         if (hit)
         {
             hit.transform.gameObject.TryGetComponent(out EntityController EntityC);
@@ -40,5 +43,6 @@ public class TaskAttackRaycast : MonoBehaviour
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(HitTransform.position, AttackRadius);
+        Gizmos.DrawRay(hit.point,hit.normal);
     }
 }
